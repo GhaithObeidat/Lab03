0620c344ff235669dc67302ff2c6a0f23ea0c5df
Ghaith Obeidat <ghaith.ob85@gmail.com>
2021-03-07 15:35:38 +0200

    update

---
+++ app.js
@@ -0,0 +1,23 @@
+var name = prompt('enter your name');
+alert("Welcome " + name);
+confirm("This is our first website demo");
+var color = prompt('What color do you prefer : red, blue, green?');
+if (color == 'red') {
+    document.body.innerText = String.fromCodePoint(0x1F600);
+    document.write('<h1 style="color:red;">Welcome to ASAC</h1>');
+}
+else if (color == "blue") {
+    document.body.innerText = String.fromCodePoint(0x1F600);
+    document.write('<h1 style="color:blue;">Welcome to ASAC</h1>');
+}
+else if (color == 'green') {
+    document.body.innerText = String.fromCodePoint(0x1F600);
+    document.write('<h1 style="color:green;">Welcome to ASAC</h1>');
+}
+else {
+    document.body.innerText = String.fromCodePoint(0x1F600);
+    document.write('<h1>Welcome to ASAC</h1>');
+}
+
+
+
